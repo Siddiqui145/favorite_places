57842618314aef51bfc6f97e4f282ffa418bb64b
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-13 16:25:09 +0530

    setting up riverpod for adding places

---
+++ lib/providers/places_provider.dart
@@ -0,0 +1,13 @@
+import 'package:favorite_places/models/place_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<PlaceModel>> {
+  UserPlacesNotifier(): super(const []);
+
+  void addPlace(String title) {
+    final newPlace = PlaceModel(title: title);
+    state = [newPlace, ...state]; //new place added would always be first appended, wouldn't be replaced
+  }
+}
+
+final userPlacesProvider = StateNotifierProvider((ref) => UserPlacesNotifier());
