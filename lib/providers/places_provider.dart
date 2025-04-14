863208b31cb9f9eff138b1dbcbf15c90b8db58df
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-14 14:34:14 +0530

    saved image into our local storage, with provider & added path

--- lib/providers/places_provider.dart
+++ lib/providers/places_provider.dart
@@ -1,13 +1,20 @@
 import 'dart:io';
-
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspaths; 
 import 'package:favorite_places/models/place_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<PlaceModel>> {
   UserPlacesNotifier(): super(const []);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = PlaceModel(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+
+    // just storing the image data properly on our device
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
+    final newPlace = PlaceModel(title: title, image: copiedImage, location: location);
     state = [newPlace, ...state]; //new place added would always be first appended, wouldn't be replaced
   }
 }
