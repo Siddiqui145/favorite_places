c404ebf830703229b47f5ae7150730ed048c2d22
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-14 09:58:09 +0530

    inputting the location, added loc dependency

--- lib/widgets/location_input_widget.dart
+++ lib/widgets/location_input_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+// AIzaSyBoGcwaQUSmNgcesK-6aoJzNNihJiAq6pI
 
 class LocationInputWidget extends StatefulWidget {
   const LocationInputWidget({super.key});
@@ -8,8 +11,59 @@ class LocationInputWidget extends StatefulWidget {
 }
 
 class _LocationInputWidgetState extends State<LocationInputWidget> {
+  Location? pickedLocation;
+  var isgettingLocation = false;
+
+  void getCurrentLocation() async {
+
+    
+
+    Location location = Location();
+
+bool serviceEnabled;
+PermissionStatus permissionGranted;
+LocationData locationData;
+
+serviceEnabled = await location.serviceEnabled();
+if (!serviceEnabled) {
+  serviceEnabled = await location.requestService();
+  if (!serviceEnabled) {
+    return;
+  }
+}
+
+permissionGranted = await location.hasPermission();
+if (permissionGranted == PermissionStatus.denied) {
+  permissionGranted = await location.requestPermission();
+  if (permissionGranted != PermissionStatus.granted) {
+    return;
+  }
+}
+
+    setState(() {
+      isgettingLocation = true;
+    });
+
+      locationData = await location.getLocation();
+
+    setState(() {
+      isgettingLocation = false;
+    });
+}
+
   @override
   Widget build(BuildContext context) {
+
+    Widget previewContent = Text('No Location chosen',
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onSurface
+            ),);
+
+    if (isgettingLocation)            {
+      previewContent = const CircularProgressIndicator();
+    }
+
     return SingleChildScrollView(
       child: Column(
         children: [
@@ -23,17 +77,13 @@ class _LocationInputWidgetState extends State<LocationInputWidget> {
                 color: Theme.of(context).colorScheme.primary.withValues(alpha: 0.2)
               )
             ),
-            child: Text('No Location chosen',
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-              color: Theme.of(context).colorScheme.onSurface
-            ),),
+            child: previewContent
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               TextButton.icon(
-                onPressed: () {}, 
+                onPressed: getCurrentLocation, 
                 label: Text('Get User Location'),
                 icon: Icon(Icons.location_on),),
 
